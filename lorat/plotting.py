238c963fcbc53478f207277eed01a73fbac93917
dragosbogatu <drabo@byg.dtu.dk>
2021-07-23 15:22:33 +0200

    updated plotting script

--- lorat/plotting.py
+++ lorat/plotting.py
@@ -1,25 +1,40 @@
 import matplotlib.pyplot as plt
 
-def graph3D(x, y, z):
+def graph(x, y, z, dt, N):
     print("x_0: ", x[0], "    y_0:  ", y[0], "    z_0:   ", z[0])
     print("x_f: ", x[len(x)-1], "    y_f:  ", y[len(x)-1], "    z_f:   ", z[len(x)-1])
-    fig = plt.figure()
-    ax = fig.gca(projection="3d")
-    ax.plot(x, y, z, lw = 0.5)
-    ax.set_xlabel("x")
-    ax.set_ylabel("y")
-    ax.set_zlabel("z")
-    ax.set_title("Lorenz Attractor")
 
+    fig = plt.figure(figsize = (6, 6))
+    gs = fig.add_gridspec(ncols=7, nrows=3, wspace=1, hspace = 0.5)
     
+    graph3D = fig.add_subplot(gs[0:, :3], projection='3d')
+    xy = fig.add_subplot(gs[0, 4:])
+    yz = fig.add_subplot(gs[1, 4:])
+    xz = fig.add_subplot(gs[2, 4:])
+    
+    
+    #fig, ((ax1, ax2), (ax3, ax4, ax5)) = plt.subplots(2, 3)
+    
+   # graph3D.plot(x, y, z, lw = 0.5)
 
-def graph2D(x, y, z):
+    s=10
+    cmap = plt.cm.viridis
+    for i in range(0,N-s,s):
+        graph3D.plot(x[i:i+s+1], y[i:i+s+1], z[i:i+s+1], color=cmap(i/N), alpha=0.4)
+        xy.plot(x[i:i+s+1], y[i:i+s+1], color=cmap(i/N), alpha=0.4)
+        yz.plot(y[i:i+s+1], z[i:i+s+1], color=cmap(i/N), alpha=0.4)
+        xz.plot(x[i:i+s+1], z[i:i+s+1], color=cmap(i/N), alpha=0.4)
 
-    plt.subplot(131)
-    plt.plot(x,y)
-    plt.subplot(132)
-    plt.plot(y,z)
-    plt.subplot(133)
-    plt.plot(x,z)
+    graph3D.set_xlabel("x")
+    graph3D.set_ylabel("y")
+    graph3D.set_zlabel("z")
+    graph3D.set_title("Lorenz Attractor")
+    
+    xy.set_xlabel("x")
+    xy.set_ylabel("y")
 
-    return
+    yz.set_xlabel("y")
+    yz.set_ylabel("z")
+
+    xz.set_xlabel("x")
+    xz.set_ylabel("z")   
