64d4710298add6e1eb4be4206fc50c3b43040bde
dragosbogatu <drabo@byg.dtu.dk>
2021-07-26 16:16:00 +0200

    Updated gui interface

--- lorat/gui.py
+++ lorat/gui.py
@@ -9,26 +9,36 @@ from matplotlib.figure import Figure
 
 import numpy as np
 
+font = {'family' : 'Arial',
+        'size' : '11'}
+
 N = np.linspace(start = 500, stop = 50000, num = 100, dtype = int)
 dt = np.linspace(start = 0.01, stop = 0.0001, num = 100)
-x = np.linspace(start = 0.1, stop = 1000, num = 100)
-y = np.linspace(start = 0.00001, stop = 1, num = 100)
-z = np.linspace(start = 0.001, stop = 10, num = 100)
+x = np.linspace(start = 0, stop = 10, num = 100)
+y = np.linspace(start = 0, stop = 10, num = 100)
+z = np.linspace(start = 0, stop = 10, num = 100)
 root = tkinter.Tk()
 root.wm_title("Embedding in Tk")
 sigma = [10, 10, 10, 14, 14]
 betta = [8/3, 8/3, 8/3, 8/3, 13/3]
+beta_str = ["8/3", "8/3", "8/3", "8/3", "13/3"] #generate string to be able to show ratio
 ro = [6, 16, 28, 28, 28]
-fig = Figure(figsize=(15, 10), dpi=100)
+
+fig = Figure(figsize=(10, 5), dpi=100)
 t = np.arange(0, 3, .01)
 subplots = []
+
 subplots.append(fig.add_subplot(3, 2, 1, projection = '3d'))
 subplots.append(fig.add_subplot(3, 2, 2, projection = '3d'))
 subplots.append(fig.add_subplot(3, 2, 3, projection = '3d'))
 subplots.append(fig.add_subplot(3, 2, 4, projection = '3d'))
 subplots.append(fig.add_subplot(3, 2, 5, projection = '3d'))
 
-
+for (subplot, index_s, index_b, index_r) in zip(subplots, sigma, beta_str, ro):
+    subplot.set_xlabel('x')
+    subplot.set_ylabel('y')
+    subplot.set_zlabel('z')
+    subplot.set_title("(" + r'$\sigma$, ' + r'$\beta$, ' + r'$\rho$)=' + "(" + str(index_s) + ", " + index_b + ", " + str(index_r) + ")")
 
 canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
 canvas.draw()
@@ -54,29 +64,35 @@ def _quit():
 
 
 
-button = tkinter.Button(master=root, text="Quit", command=_quit)
-scaler = tkinter.Scale(master=root, from_=0, to=100, orient=tkinter.HORIZONTAL)
-scaler_2 = tkinter.Scale(master=root, from_=0, to=100, orient=tkinter.HORIZONTAL)
-scaler_3 = tkinter.Scale(master=root, from_=0, to=100, orient=tkinter.HORIZONTAL)
-scaler_4 = tkinter.Scale(master=root, from_=0, to=100, orient=tkinter.HORIZONTAL)
-scaler_5 = tkinter.Scale(master=root, from_=0, to=100, orient=tkinter.HORIZONTAL)
+button = tkinter.Button(master=root, text="Quit", command=_quit, width = 20)
+scaler = tkinter.Scale(master=root, from_=0, to=100, orient=tkinter.HORIZONTAL, width = 20)
+txt = tkinter.StringVar()
+txt.set('dt')
+label = tkinter.Label(textvariable = txt)
+scaler_2 = tkinter.Scale(master=root, from_=0, to=100, orient=tkinter.HORIZONTAL, width = 20)
+scaler_3 = tkinter.Scale(master=root, from_=0, to=100, orient=tkinter.HORIZONTAL, width = 20)
+scaler_4 = tkinter.Scale(master=root, from_=0, to=100, orient=tkinter.HORIZONTAL, width = 20)
 
 def updateValue(event):
     value = int(scaler.get())
     value_2 = int(scaler_2.get())
     value_3 = int(scaler_3.get())
     value_4 = int(scaler_4.get())
-    value_5 = int(scaler_5.get())
 
-    print("dt: = ", dt[value], "    N: = ", N[value_2],   "  x: = ", x[value_3], "   y= :  ", y[value_4],  "z=  :    ", z[value_5])
-    for (subplot, sig, r, bet) in zip(subplots, sigma, ro, betta):
+    print("dt: = ", dt[value], "    N: = ", N[value],   "  x: = ", x[value_2], "   y= :  ", y[value_3],  "z=  :    ", z[value_4])
+    txt.set("dt: = " + str(round(dt[value],4)) + "    N: = " + str(N[value]))
+    for (subplot, sig, r, bet, bstr) in zip(subplots, sigma, ro, betta, beta_str):
 
-        x_start = [x[value_3]]; y_start = [y[value_4]]; z_start = [z[value_5]]
-        s, t, u = lorenz.euler(x_start, y_start, z_start, sig, r, bet, dt[value], N[value_2])
+        x_start = [x[value_2]]; y_start = [y[value_3]]; z_start = [z[value_4]]
+        s, t, u = lorenz.euler(x_start, y_start, z_start, sig, r, bet, dt[value], N[value])
         subplot.clear()    
-    
         subplot.plot(s, t, u, lw=0.5)
-    #subplot_2.plot(t, value * np.sin(value * np.pi * t))
+        subplot.set_xlabel('x')
+        subplot.set_ylabel('y')
+        subplot.set_zlabel('z')
+        subplot.set_title("(" + r'$\sigma$, ' + r'$\beta$, ' + r'$\rho$)=' + "(" + str(sig) + ", " + bstr + ", " + str(r) + ")")
+ 
+     #subplot_2.plot(t, value * np.sin(value * np.pi * t))
     canvas.draw()
     
 
@@ -84,14 +100,13 @@ scaler.bind("<ButtonRelease-1>", updateValue)
 scaler_2.bind("<ButtonRelease-1>", updateValue)
 scaler_3.bind("<ButtonRelease-1>", updateValue)
 scaler_4.bind("<ButtonRelease-1>", updateValue)
-scaler_5.bind("<ButtonRelease-1>", updateValue)
 
-button.pack(side=tkinter.BOTTOM)
-scaler.pack(side=tkinter.BOTTOM)
-scaler_2.pack(side=tkinter.BOTTOM)
-scaler_3.pack(side=tkinter.BOTTOM)
-scaler_4.pack(side=tkinter.BOTTOM)
-scaler_5.pack(side=tkinter.BOTTOM)
+button.pack(padx = 130, side=tkinter.LEFT)
+label.pack(side = tkinter.LEFT)
+scaler.pack(side=tkinter.LEFT)
+scaler_2.pack(padx = 50, side=tkinter.RIGHT)
+scaler_3.pack(padx = 50, side=tkinter.RIGHT)
+scaler_4.pack(padx = 50, side=tkinter.RIGHT)
 tkinter.mainloop()
 # If you put root.destroy() here, it will cause an error if the window is
 # closed with the window manager.
