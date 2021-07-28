fd42187284301a4d4e51c005e09326b8339d2b02
MateiSarivan <s.i.matei96@gmail.com>
2021-07-28 17:44:57 +0200

    hopes travis works now

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -2,10 +2,10 @@
 PKG_NAME=lorat
 USER=mateisarivan
 
-OS=$TRAVIS_OS_NAME-64
+OS=linux-64
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m.%d`
 conda build .
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 --force
