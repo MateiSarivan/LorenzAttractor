6565bda44ce85d20b36d87e0dc65688d1111ad88
MateiSarivan <s.i.matei96@gmail.com>
2021-07-28 08:32:01 +0200

    Transformed lorat gui into class; code cleanup

---
+++ lorat/lorat_gui.py
@@ -0,0 +1,222 @@
+import os
+import tkinter
+import lorenz
+import timeit
+import csv
+import json
+import fractions
+import numpy as np
+
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+
+from plotting import graph
+from pdf_gen import generate_pdf
+from pdf_merge import merge_pdfs
+
+class LorenzGUI:
+    def __init__(self):
+        
+        self.file_address = None
+        self.experiment_data = {}
+
+        json_file = open('configuration.json',)
+        data = json.load(json_file)
+        self.range_N = np.linspace(start = data['configuration']['N']['min'], stop = data['configuration']['N']['max'], num = 100, dtype = int)
+        self.range_dt = np.linspace(start = data['configuration']['dt']['max'], stop = data['configuration']['dt']['min'], num = 100)
+        self.range_initial_x = np.linspace(start = data['configuration']['x']['min'], stop = data['configuration']['x']['max'], num = 100)
+        self.range_initial_y = np.linspace(start = data['configuration']['y']['min'], stop = data['configuration']['y']['max'], num = 100)
+        self.range_initial_z = np.linspace(start = data['configuration']['z']['min'], stop = data['configuration']['z']['max'], num = 100)
+        self.list_sigma = np.float_(data['configuration']['sigma'])
+        self.list_beta = [float(fractions.Fraction(x)) for x in data['configuration']['beta']] #transform beta to float
+        self.list_rho = np.float_(data['configuration']['rho'])
+        self.list_beta_string = data['configuration']['beta']
+        json_file.close()
+
+
+        root = tkinter.Tk()
+        root.geometry("1366x768")
+        root.wm_title("Embedding in Tk")
+
+        fig = Figure(figsize=(10, 5), dpi=100)
+        self.subplots = []
+        self.subplots.append(fig.add_subplot(2, 3, 1, projection = '3d'))
+        self.subplots.append(fig.add_subplot(2, 3, 2, projection = '3d'))
+        self.subplots.append(fig.add_subplot(2, 3, 3, projection = '3d'))
+        self.subplots.append(fig.add_subplot(2, 3, 4, projection = '3d'))
+        self.subplots.append(fig.add_subplot(2, 3, 5, projection = '3d'))
+
+        for (subplot, sigma, beta, rho) in zip(self.subplots, self.list_sigma, self.list_beta_string, self.list_rho):
+            subplot.set_xlabel('x')
+            subplot.set_ylabel('y')
+            subplot.set_zlabel('z')
+            subplot.set_title("(" + r'$\sigma$, ' + r'$\beta$, ' + r'$\rho$)=' + "(" + str(sigma) + ", " + beta + ", " + str(rho) + ")")
+
+
+        self.canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
+        self.canvas.draw()
+        self.canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+        toolbar = NavigationToolbar2Tk(self.canvas, root)
+        toolbar.update()
+        self.canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+        def on_key_press(event):
+            print("you pressed {}".format(event.key))
+            key_press_handler(event, self.canvas, toolbar)
+
+        self.canvas.mpl_connect("key_press_event", on_key_press)
+
+        self.txt_N_dt = tkinter.StringVar()
+        self.txt_x = tkinter.StringVar()
+        self.txt_y = tkinter.StringVar()
+        self.txt_z = tkinter.StringVar()
+
+        self.txt_N_dt.set("dt = " + str(round(self.range_dt[0],4)) + "    N = " + str(self.range_N[0]))
+        self.txt_x.set("x = " + str(np.round(self.range_initial_x[0],3)))
+        self.txt_y.set("y = " + str(np.round(self.range_initial_y[0],3)))
+        self.txt_z.set("z = " + str(np.round(self.range_initial_z[0],3)))
+
+        self.scaler_dt_N = tkinter.Scale(master=root, from_=1, to=100, orient=tkinter.HORIZONTAL, width = 20, command = self.update_sc, bg='#E3BA7C')
+        
+
+        label = tkinter.Label(textvariable = self.txt_N_dt, width = 25, bg='#E3BA7C')
+        label_x = tkinter.Label(textvariable = self.txt_x, width = 14, bg='#D02A1E')
+        label_y = tkinter.Label(textvariable = self.txt_y, width = 14, bg='#00C753')
+        label_z = tkinter.Label(textvariable = self.txt_z, width = 14, bg='#0CB1F2')
+        self.scaler_2 = tkinter.Scale(master=root, from_=1, to=100, orient=tkinter.HORIZONTAL, width = 20, command = self.update_scx, bg='#D02A1E')
+        self.scaler_3 = tkinter.Scale(master=root, from_=1, to=100, orient=tkinter.HORIZONTAL, width = 20, command = self.update_scy, bg='#00C753')
+        self.scaler_4 = tkinter.Scale(master=root, from_=1, to=100, orient=tkinter.HORIZONTAL, width = 20, command = self.update_scz, bg='#0CB1F2')
+
+
+        self.scaler_dt_N.bind("<ButtonRelease-1>", self.updateValue)
+        self.scaler_2.bind("<ButtonRelease-1>", self.updateValue)
+        self.scaler_3.bind("<ButtonRelease-1>", self.updateValue)
+        self.scaler_4.bind("<ButtonRelease-1>", self.updateValue)
+
+        button = tkinter.Button(master=root, text="Save ", command=self._save, width = 20, bg= '#8DA696')
+
+        #root.geometry('250x200+250+200')
+        button.place(x=1550, y=750)
+
+        self.scaler_dt_N.place(x=1320, y=800)
+        label.place(x=1275, y=750)
+
+        self.scaler_2.place(x=1250, y=650)
+        label_x.place(x=1250, y=600)
+
+        self.scaler_3.place(x=1450, y=650)
+        label_y.place(x=1450, y=600)
+
+        self.scaler_4.place(x=1650, y=650)
+        label_z.place(x=1650, y=600)
+
+        self.updateValue('event')
+
+        tkinter.mainloop()
+
+
+    def update_sc(self, event):
+        self.txt_N_dt.set("dt: = " + str(round(self.range_dt[int(event)-1],4)) + "    N: = " + str(self.range_N[int(event)-1]))
+
+    def update_scx(self, event):
+        self.txt_x.set("x = " + str(np.round(self.range_initial_x[int(event)], 3)))
+
+    def update_scy(self, event):
+        self.txt_y.set("y = " + str(np.round(self.range_initial_y[int(event)], 3)))
+
+    def update_scz(self, event):
+        self.txt_z.set("z = " + str(np.round(self.range_initial_z[int(event)], 3)))
+
+
+
+    def updateValue(self, event):
+
+        value = int(self.scaler_dt_N.get()) - 1
+        value_2 = int(self.scaler_2.get()) - 1
+        value_3 = int(self.scaler_3.get()) - 1
+        value_4 = int(self.scaler_4.get()) - 1
+
+        self.experiment_data = {}
+        self.experiment_data["init_x"] = self.range_initial_x[value_2]
+        self.experiment_data["init_y"] = self.range_initial_y[value_3]
+        self.experiment_data["init_z"] = self.range_initial_z[value_4]
+        self.experiment_data["dt"] = self.range_dt[value]
+        self.experiment_data["N"] = self.range_N[value]
+        self.experiment_data["beta"] = self.list_beta
+        self.experiment_data["sigma"] = self.list_sigma
+        self.experiment_data["rho"] = self.list_rho
+        self.experiment_data["data"] = []
+        time_start = timeit.default_timer()
+        for (sig, r, bet) in zip(self.list_sigma, self.list_rho, self.list_beta):
+
+            x_start = [self.range_initial_x[value_2]]; y_start = [self.range_initial_y[value_3]]; z_start = [self.range_initial_z[value_4]]
+            sampling_start_time = timeit.default_timer()
+            x_sampled, y_sampled, z_sampled = lorenz.euler(x_start, y_start, z_start, sig, bet, r, self.range_dt[value], self.range_N[value])
+            sampling_time = timeit.default_timer() - sampling_start_time
+            self.experiment_data["data"].append([x_sampled, y_sampled, z_sampled, sampling_time])
+
+        self.experiment_data["elapsed_time"] = timeit.default_timer() - time_start
+
+        for (subplot, data_set, bstr, sig, r) in zip(self.subplots, self.experiment_data["data"], self.list_beta_string, self.list_sigma, self.list_rho):
+            subplot.clear()    
+            subplot.plot(data_set[0], data_set[1], data_set[2], lw=0.5)
+            subplot.set_xlabel('x')
+            subplot.set_ylabel('y')
+            subplot.set_zlabel('z')
+            subplot.set_title("(" + r'$\sigma$, ' + r'$\beta$, ' + r'$\rho$)=' + "(" + str(sig) + ", " + bstr + ", " + str(r) + ")")
+    
+        #subplot_2.plot(t, value * np.sin(value * np.pi * t))
+        self.canvas.draw()
+
+
+
+    def _save(self):
+        
+        if self.file_address is None or len(self.file_address) == 0:
+            self.file_address = tkinter.filedialog.askdirectory()
+            if "LoratResults" not in self.file_address and len(self.file_address):
+                self.file_address = os.path.join(self.file_address, "LoratResults")
+                if not os.path.exists(self.file_address):
+                    os.makedirs(self.file_address)
+
+        if len(self.file_address):
+
+            experiment_name = ';'.join([
+                "N&dt=" + str(self.scaler_dt_N.get()),
+                "x=" + str(self.scaler_2.get()),
+                "y=" + str(self.scaler_3.get()),
+                "z=" + str(self.scaler_3.get())
+            ])
+            experiment_address = os.path.join(self.file_address, experiment_name)
+            if not os.path.exists(experiment_address):
+                os.makedirs(experiment_address)
+            
+            np.save(os.path.join(experiment_address, experiment_name + ".npy"), self.experiment_data)
+            generate_pdf(os.path.join(experiment_address, "gendata" + ".pdf"), self.experiment_data["init_x"], self.experiment_data["init_y"], self.experiment_data["init_z"], self.experiment_data["N"], self.experiment_data["dt"], self.experiment_data["elapsed_time"])
+            f = open(os.path.join(experiment_address, experiment_name + ".csv"), 'w', newline="")
+            csvw = csv.writer(f, delimiter=",")
+            csvw.writerow(["init x", "init y", "init z", "N", "dt", "elapsed_time_total"])
+            csvw.writerow([self.experiment_data["init_x"], self.experiment_data["init_y"], self.experiment_data["init_z"], self.experiment_data["N"], self.experiment_data["dt"], self.experiment_data["elapsed_time"]])
+            i = 0
+            for (data_set, sigma, beta, rho, beta_string) in zip(self.experiment_data["data"], self.list_sigma, self.list_beta, self.list_rho, self.list_beta_string):
+                csvw.writerow(["beta", "sigma", "rho", "time_elapsed"])
+                csvw.writerow([sigma, beta, rho, data_set[3]])
+                csvw.writerow(["x", "y", "z"])
+                graph_name = ';'.join([experiment_name,
+                    "S=" + str(i),
+                    "B=" + str(i),
+                    "R=" + str(i)
+                ])
+                i += 1
+                graph(data_set[0], data_set[1], data_set[2], self.experiment_data["dt"], int(self.experiment_data["N"]), sigma, beta_string, rho, data_set[3], os.path.join(experiment_address, graph_name + ".png"))
+                graph(data_set[0], data_set[1], data_set[2], self.experiment_data["dt"], int(self.experiment_data["N"]), sigma, beta_string, rho, data_set[3], os.path.join(experiment_address, graph_name + ".pdf"))
+                for (x, y, z) in zip(data_set[0], data_set[1], data_set[2]):
+                    csvw.writerow([x, y, z])
+            f.close()
+
+            merge_pdfs(experiment_address, experiment_name)
+            
+LorenzGUI()
--- lorat/result_explorer.py
+++ lorat/result_explorer.py
@@ -4,146 +4,149 @@ import os
 import numpy as np
 from plotting import graph
 
+class ResultExplorer:
+    def __init__(self):
 
 
-file_address = None
-experiment = {}
-  
-# Creating the root window
-root = tkinter.Tk()
-root.geometry("450x300")
+        self.file_address = None
+        self.experiment = {}
+        self.button_state = False
+        
+        root = tkinter.Tk()
+        root.geometry("450x300")
 
-  
-# Creating a Listbox and
-# attaching it to root window
+        frame_1 = Frame(root)
+        frame_2 = Frame(root)
+        frame_3 = Frame(frame_2)
 
-frame_1 = Frame(root)
-frame_2 = Frame(root)
-frame_3 = Frame(frame_2)
+        txt_x_initial = tkinter.StringVar()
+        txt_y_initial = tkinter.StringVar()
+        txt_z_initial = tkinter.StringVar()
+        txt_dt = tkinter.StringVar()
+        txt_N = tkinter.StringVar()
+        listbox_2 = Listbox(frame_3, height=5, width=25)
+        scrollbar_2 = Scrollbar(frame_3)
+        scrollbar_2.pack(side = LEFT, fill = BOTH)
+        listbox_2.config(yscrollcommand = scrollbar_2.set)
+        scrollbar_2.config(command = listbox_2.yview)
 
-txt_x_initial = tkinter.StringVar()
-txt_y_initial = tkinter.StringVar()
-txt_z_initial = tkinter.StringVar()
-txt_dt = tkinter.StringVar()
-txt_N = tkinter.StringVar()
-listbox_2 = Listbox(frame_3, height=5, width=25)
-scrollbar_2 = Scrollbar(frame_3)
-scrollbar_2.pack(side = LEFT, fill = BOTH)
-listbox_2.config(yscrollcommand = scrollbar_2.set)
-scrollbar_2.config(command = listbox_2.yview)
-def list_select(event):
-    global file_address, experiment
-    data = None
-    selection = event.widget.curselection()
-    if selection:
-        index = selection[0]
-        data = event.widget.get(index)
-    if data is not None:
-        listbox_2.delete(0, END)
-        experiment_address = os.path.join(file_address, data, data + ".npy")
-        print(experiment_address)
-        experiment_data = np.load(experiment_address, allow_pickle=True)
-        txt_x_initial.set("Initial x: " + str(experiment_data.item().get("init_x")))
-        experiment["x"] = experiment_data.item().get("init_x")
-        txt_y_initial.set("Initial y: " + str(experiment_data.item().get("init_y")))
-        experiment["y"] = experiment_data.item().get("init_y")
-        txt_z_initial.set("Initial z: " + str(experiment_data.item().get("init_z")))
-        experiment["z"] = experiment_data.item().get("init_z")
-        txt_dt.set("dt: " + str(experiment_data.item().get("dt")))
-        experiment["dt"] = experiment_data.item().get("dt")
-        txt_N.set("N: " + str(experiment_data.item().get("N")))
-        experiment["N"] = experiment_data.item().get("N")
-        beta_parameters = experiment_data.item().get("beta")
-        experiment["beta"] = beta_parameters
-        sigma_parameters = experiment_data.item().get("sigma")
-        experiment["sigma"] = sigma_parameters
-        rho_parameters = experiment_data.item().get("rho")
-        experiment["rho"] = rho_parameters
-        experiment["data"] = experiment_data.item().get("data")
-        print(beta_parameters)
+        button = tkinter.Button(master=frame_2, text="Plot", command=_plot, width = 20, bg= '#8DA696')
+        button['state'] = 'disabled'
 
-        for (sigma, beta, rho) in zip(sigma_parameters, beta_parameters, rho_parameters):
-            listbox_2.insert(END, "σ=" + str(np.round(sigma, 3)) + ";  β=" + str(np.round(beta, 3)) + ";  ρ=" + str(rho))
+        listbox = Listbox(frame_1, width = 35)
+        listbox.bind("<<ListboxSelect>>", list_select)
+        listbox_2.bind("<<ListboxSelect>>", list_select_2)
+
+        label_x_initial = tkinter.Label(master=frame_2, textvariable = txt_x_initial, width = 25, bg='#E3BA7C')
+        label_y_initial = tkinter.Label(master=frame_2, textvariable = txt_y_initial, width = 25, bg='#E3BA7C')
+        label_z_initial = tkinter.Label(master=frame_2, textvariable = txt_z_initial, width = 25, bg='#E3BA7C')
+        label_dt = tkinter.Label(master=frame_2, textvariable = txt_dt, width = 25, bg='#E3BA7C')
+        label_N = tkinter.Label(master=frame_2, textvariable = txt_N, width = 25, bg='#E3BA7C')
+
+        listbox_2.pack(side=TOP, fill="y", expand=True)
+
+        label_x_initial.pack()
+        label_y_initial.pack()
+        label_z_initial.pack()
+        label_dt.pack()
+        label_N.pack()
+        frame_3.pack()
+
+        button.pack()
+
+        frame_2.pack(side = RIGHT, fill="x", expand=True)
+
+        listbox.pack(side = RIGHT, fill = BOTH)
+        
+        # Creating a Scrollbar and 
+        # attaching it to root window
+        scrollbar = Scrollbar(frame_1)
+        
+        # Adding Scrollbar to the right
+        # side of root window
+        scrollbar.pack(side = LEFT, fill = BOTH)
+        
+        # Insert elements into the listbox
+            
+        # Attaching Listbox to Scrollbar
+        # Since we need to have a vertical 
+        # scroll we use yscrollcommand
+        listbox.config(yscrollcommand = scrollbar.set)
+        
+        # setting scrollbar command parameter 
+        # to listbox.yview method its yview because
+        # we need to have a vertical view
+        scrollbar.config(command = listbox.yview)
+
+        frame_1.pack(side = LEFT, fill="y")
+
+        root.after(100, get_address)
+        root.mainloop()
 
 
-def list_select_2(event):
-    global experiment
-    selection = event.widget.curselection()
-    if selection:
-        index = selection[0]
-        print(index)
-        experiment["index_constants"] = index
+    def _plot():
+        index = self.experiment["index_constants"]
+        xyz = self.experiment["data"][index]
+        graph(xyz[0], xyz[1], xyz[2], self.experiment["dt"], self.experiment["N"], self.experiment["sigma"][index], self.experiment["beta"][index], self.experiment["rho"][index], xyz[3])
+
+
+    def list_select(event):
+        data = None
+        selection = event.widget.curselection()
+        if selection:
+            index = selection[0]
+            data = event.widget.get(index)
+        if data is not None:
+            listbox_2.delete(0, END)
+            experiment_address = os.path.join(self.file_address, data, data + ".npy")
+            print(experiment_address)
+            experiment_data = np.load(experiment_address, allow_pickle=True)
+            txt_x_initial.set("Initial x: " + str(experiment_data.item().get("init_x")))
+            self.experiment["x"] = experiment_data.item().get("init_x")
+            txt_y_initial.set("Initial y: " + str(experiment_data.item().get("init_y")))
+            self.experiment["y"] = experiment_data.item().get("init_y")
+            txt_z_initial.set("Initial z: " + str(experiment_data.item().get("init_z")))
+            self.experiment["z"] = experiment_data.item().get("init_z")
+            txt_dt.set("dt: " + str(experiment_data.item().get("dt")))
+            self.experiment["dt"] = experiment_data.item().get("dt")
+            txt_N.set("N: " + str(experiment_data.item().get("N")))
+            self.experiment["N"] = experiment_data.item().get("N")
+            beta_parameters = experiment_data.item().get("beta")
+            self.experiment["beta"] = beta_parameters
+            sigma_parameters = experiment_data.item().get("sigma")
+            self.experiment["sigma"] = sigma_parameters
+            rho_parameters = experiment_data.item().get("rho")
+            self.experiment["rho"] = rho_parameters
+            self.experiment["data"] = experiment_data.item().get("data")
+
+            self.button_state = True        
+
+            for (sigma, beta, rho) in zip(sigma_parameters, beta_parameters, rho_parameters):
+                listbox_2.insert(END, "σ=" + str(np.round(sigma, 3)) + ";  β=" + str(np.round(beta, 3)) + ";  ρ=" + str(rho))
+
+
+    def list_select_2(event):
+        selection = event.widget.curselection()
+        if selection:
+            index = selection[0]
+            print(index)
+            self.experiment["index_constants"] = index
+
+        if self.button_state:
+            button['state'] = "normal"
         
 
-listbox = Listbox(frame_1, width = 35)
-listbox.bind("<<ListboxSelect>>", list_select)
-listbox_2.bind("<<ListboxSelect>>", list_select_2)
-# Adding Listbox to the left
-# side of root window
 
 
-label_x_initial = tkinter.Label(master=frame_2, textvariable = txt_x_initial, width = 25, bg='#E3BA7C')
-label_y_initial = tkinter.Label(master=frame_2, textvariable = txt_y_initial, width = 25, bg='#E3BA7C')
-label_z_initial = tkinter.Label(master=frame_2, textvariable = txt_z_initial, width = 25, bg='#E3BA7C')
-label_dt = tkinter.Label(master=frame_2, textvariable = txt_dt, width = 25, bg='#E3BA7C')
-label_N = tkinter.Label(master=frame_2, textvariable = txt_N, width = 25, bg='#E3BA7C')
+    def get_address():
 
-listbox_2.pack(side=TOP, fill="y", expand=True)
+        self.file_address = tkinter.filedialog.askdirectory()
+        if "LoratResults" not in self.file_address:
 
-label_x_initial.pack()
-label_y_initial.pack()
-label_z_initial.pack()
-label_dt.pack()
-label_N.pack()
-frame_3.pack()
-def _plot():
-    index = experiment["index_constants"]
-    xyz = experiment["data"][index]
-    graph(xyz[0], xyz[1], xyz[2], experiment["dt"], experiment["N"], experiment["sigma"][index], experiment["beta"][index], experiment["rho"][index], xyz[3])
-
-button = tkinter.Button(master=frame_2, text="Plot", command=_plot, width = 20, bg= '#8DA696')
-button.pack()
-
-frame_2.pack(side = RIGHT, fill="x", expand=True)
+            root.quit()
+        else:
+            experiments_names = os.listdir(self.file_address)
+            for experiment_name in experiments_names:
+                listbox.insert(tkinter.END, experiment_name)
 
 
-listbox.pack(side = RIGHT, fill = BOTH)
-  
-# Creating a Scrollbar and 
-# attaching it to root window
-scrollbar = Scrollbar(frame_1)
-  
-# Adding Scrollbar to the right
-# side of root window
-scrollbar.pack(side = LEFT, fill = BOTH)
-  
-# Insert elements into the listbox
-      
-# Attaching Listbox to Scrollbar
-# Since we need to have a vertical 
-# scroll we use yscrollcommand
-listbox.config(yscrollcommand = scrollbar.set)
-  
-# setting scrollbar command parameter 
-# to listbox.yview method its yview because
-# we need to have a vertical view
-scrollbar.config(command = listbox.yview)
-
-frame_1.pack(side = LEFT, fill="y")
-
-
-def get_address():
-    global file_address
-
-    file_address = tkinter.filedialog.askdirectory()
-    if "LoratResults" not in file_address:
-
-        root.quit()
-    else:
-        experiments_names = os.listdir(file_address)
-        for experiment_name in experiments_names:
-            listbox.insert(tkinter.END, experiment_name)
-
-
-root.after(100, get_address)
-root.mainloop()
