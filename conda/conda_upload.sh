bf65895c8db5a8cd6032a18c112bdf7c404b7492
MateiSarivan <s.i.matei96@gmail.com>
2021-07-28 20:30:47 +0200

    made for travis

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -8,4 +8,4 @@ conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION="0.0.7"
 conda build -c conda-forge .
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_BLD_PATH/$OS/$PKG_NAME-py38-0.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-py39hfeaa757-1.tar.bz2 --force
